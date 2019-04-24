974687be78798f407ba1e3194ce6c71e4317ca0f
yrinda <yrinda@gmail.com>
2019-04-25 00:14:09 +0900

    add components (special-thanks)

---
+++ src/app/home/special-thanks/special-thanks.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-special-thanks',
+  templateUrl: './special-thanks.component.html',
+  styleUrls: ['./special-thanks.component.sass']
+})
+export class SpecialThanksComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
